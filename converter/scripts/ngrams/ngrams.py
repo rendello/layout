5ee094a948b20fa9a912485b1aaa9795798dd8de
Gaven Rendell <gaven@rendello.ca>
2023-12-30 23:28:30 -0500

    Add prefixes and suffixes to ngram model

--- converter/scripts/ngrams/ngrams.py
+++ converter/scripts/ngrams/ngrams.py
@@ -3,43 +3,96 @@ import nltk
 from collections import Counter
 
 
-def lang_ngrams(file_name, n):
+def ngrams_f(file_name, n):
     with open(file_name, 'r') as file:
         c = Counter()
         for line in file:
-            word = line.strip()
-            word_ngrams = nltk.ngrams(word, n, pad_left=True, pad_right=True,  left_pad_symbol=' ', right_pad_symbol=' ')
-            for ng in word_ngrams:
-                c.update([''.join(ng).strip()])
+            if "'" not in line:
+                word = line.strip()
+                word_ngrams = nltk.ngrams(word, n, pad_left=True, pad_right=True,  left_pad_symbol=' ', right_pad_symbol=' ')
+                for ng in word_ngrams:
+                    c.update([''.join(ng).strip()])
         return c
 
 
-iu_ngrams = Counter()
-en_ngrams = Counter()
-fr_ngrams = Counter()
-
-for n in [1,2,3,4,5]:
-    en_ngrams += lang_ngrams("lexicon_english.txt", n)
-    fr_ngrams += lang_ngrams("lexicon_french.txt", n)
-    iu_ngrams += lang_ngrams("lexicon_inuk.txt", n)
-
-
-dissallowed_ngrams = set(en_ngrams) | set(fr_ngrams)
-
-allowed_ngrams = []
-for item, _ in iu_ngrams.most_common(1_000_000_000):
-    if item not in dissallowed_ngrams:
-        allowed_ngrams.append(item)
+def prefixes_f(file_name, n):
+    with open(file_name, 'r') as file:
+        c = Counter()
+        for line in file:
+            if "'" not in line:
+                word = line.strip()
+                c.update([word[:n]])
+        return c
 
 
-deduped = []
-for ng in sorted(sorted(allowed_ngrams), key=len):
-    for smaller_ng in deduped:
-        if smaller_ng in ng:
-            break
-    else:
-        deduped.append(ng)
 
-ngrams = [ng for ng in allowed_ngrams if ng in deduped]
-for ng in ngrams[:100]:
-    print(f'(?:{ng})|', end="")
+def suffixes_f(file_name, n):
+    with open(file_name, 'r') as file:
+        c = Counter()
+        for line in file:
+            if "'" not in line:
+                word = line.strip()
+                c.update([word[-n:]])
+        return c
+
+
+def generate_rules(sequence_function, lengths):
+    iu_sequences = Counter()
+    en_sequences = Counter()
+    fr_sequences = Counter()
+
+    for n in lengths:
+        en_sequences += sequence_function("lexicon_english.txt", n)
+        fr_sequences += sequence_function("lexicon_french.txt", n)
+        iu_sequences += sequence_function("lexicon_inuk.txt", n)
+
+    dissallowed_sequences = set(en_sequences) | set(fr_sequences)
+
+    allowed_sequences = []
+    for item, _ in iu_sequences.most_common(1_000_000_000):
+        if item not in dissallowed_sequences:
+            allowed_sequences.append(item)
+
+    deduped = []
+    for seq in allowed_sequences:
+        for smaller_seq in deduped:
+            if smaller_seq in seq:
+                break
+        else:
+            deduped.append(seq)
+
+    return sorted(sorted([seq for seq in allowed_sequences if seq in deduped][:100]), key=len)
+
+
+def filter(ngrams, sequences):
+    new_sequences = []
+    for seq in sequences:
+        for ng in ngrams:
+            if ng in seq:
+                break
+        else:
+            new_sequences.append(seq)
+    return new_sequences
+
+
+def print_all(*args):
+    for arg in args:
+        print("="*80)
+        for item in arg:
+            print(item)
+
+
+
+lengths = [1,2,3]
+
+ngrams = generate_rules(ngrams_f, lengths)
+
+prefixes = filter(ngrams, generate_rules(prefixes_f, lengths))
+suffixes = filter(ngrams, generate_rules(suffixes_f, lengths))
+
+print_all(ngrams, prefixes, suffixes)
+
+
+
+# for ng in ngrams:
+#     print(f'(?:{ng})|', end="")
