eba90839ed9bbe008d2e4dccac2a585b83db2f9e
Gaven Rendell <gaven@rendello.ca>
2024-02-07 23:24:24 -0500

    Broaden hashtable generation from Python

--- converter/tests/test.rs
+++ converter/tests/test.rs
@@ -4,7 +4,7 @@ use converter;
 use proptest::prelude::*;
 
 
-const RE: &str = r"(:?ai|i|ii|u|uu|a|aa|p|pai|pi|pii|pu|puu|pa|paa|t|tai|ti|tii|tu|tuu|ta|taa|k|kai|ki|kii|ku|kuu|ka|kaa|g|gai|gi|gii|gu|guu|ga|gaa|m|mai|mi|mii|mu|muu|ma|maa|n|nai|ni|nii|nu|nuu|na|naa|s|sai|si|sii|su|suu|sa|saa|l|lai|li|lii|lu|luu|la|laa|j|jai|ji|jii|ju|juu|ja|jaa|jj|jjai|jji|jjii|jju|jjuu|jja|jjaa|v|vai|vi|vii|vu|vuu|va|vaa|r|rai|ri|rii|ru|ruu|ra|raa|q|qai|qi|qii|qu|quu|qa|qaa|qq|qqai|qqi|qqii|qqu|qquu|qqa|qqaa|ng|ngai|ngi|ngii|ngu|nguu|nga|ngaa|nng|nngi|nngii|nngu|nnguu|nnga|nngaa|ł|łi|łii|łu|łuu|ła|łaa|b|h|ʼ)*";
+const RE: &str = r"(:?ai|i|ii|u|uu|a|aa|p|pai|pi|pii|pu|puu|pa|paa|t|tai|ti|tii|tu|tuu|ta|taa|k|kai|ki|kii|ku|kuu|ka|kaa|g|gai|gi|gii|gu|guu|ga|gaa|m|mai|mi|mii|mu|muu|ma|maa|n|nai|ni|nii|nu|nuu|na|naa|s|sai|si|sii|su|suu|sa|saa|l|lai|li|lii|lu|luu|la|laa|j|jai|ji|jii|ju|juu|ja|jaa|jj|jjai|jji|jjii|jju|jjuu|jja|jjaa|v|vai|vi|vii|vu|vuu|va|vaa|r|rai|ri|rii|ru|ruu|ra|raa|q|qai|qi|qii|qu|quu|qa|qaa|qq|qqai|qqi|qqii|qqu|qquu|qqa|qqaa|ng|ngai|ngi|ngii|ngu|nguu|nga|ngaa|nng|nngi|nngii|nngu|nnguu|nnga|nngaa|ł|łi|łii|łu|łuu|ła|łaa|b|h|ʼ| )*";
 
 proptest! {
     #![proptest_config(ProptestConfig {
@@ -30,4 +30,3 @@ fn test_main() {
     let string = String::new();
     println!("{:?}", converter::f(&maps[..], "inuktitut".as_bytes(), string));
 }
-
