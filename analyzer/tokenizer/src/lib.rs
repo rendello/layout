7bc2ae9e73d8bb932a6e7fafe8fe62fcb8094278
Gaven Rendell <gaven@rendello.ca>
2024-07-09 23:51:01 -0400

    Separate tokenizer into module; remember to actually commit it

---
+++ analyzer/tokenizer/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod tokenizer;
+
+
+
+
+// fn main() {
+//     // let s = "Here is a sample text with \nsome patterns 123 and symbols $%^&.";
+
+//     let s = r#"
+// Inuktitut (/ɪˈnʊktətʊt/ ih-NUUK-tə-tuut;[3] Inuktitut: [inuktiˈtut], syllabics ᐃᓄᒃᑎᑐᑦ; from inuk, 'person' + -titut, 'like', 'in the manner of'), also known as Eastern Canadian Inuktitut, is one of the principal Inuit languages of Canada. It is spoken in all areas north of the North American tree line, including parts of the provinces of Newfoundland and Labrador, Quebec, to some extent in northeastern Manitoba as well as the Northwest Territories and Nunavut. It is one of the aboriginal languages written with Canadian Aboriginal syllabics.[4] 
+//     "#;
+
+//     let tokens = Tokens::new(s);
+//     for t in tokens {
+//         println!("{:?}", t);
+//     }
+// }
---
+++ analyzer/tokenizer/src/tokenizer.rs
@@ -0,0 +1,58 @@
+use once_cell::sync::Lazy;
+use regex::{Regex};
+
+
+static NON_INUK_ASCII: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i)\A[a-z]*[defowxyz][a-z]*").unwrap());
+static WHITESPACE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\A\s+").unwrap());
+static SKIP: Lazy<Regex> = Lazy::new(|| Regex::new(r#"\A[!#$%&'()*+,\\/:;<=>?."\[\]0-9]+"#).unwrap());
+static OTHER: Lazy<Regex> = Lazy::new(|| Regex::new(r#"\A[^!#$%&'()*+,\\/:;<=>?."\s\[\]0-9]+"#).unwrap());
+
+
+pub struct Tokenizer<'a> {
+    buffer: &'a str
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(text: &str) -> Tokenizer {
+        Tokenizer { buffer: text }
+    }
+
+    pub fn is_consumed(&self) -> bool {
+        self.buffer.is_empty()
+    }
+}
+
+impl<'a> Iterator for Tokenizer<'a> {
+    type Item = Token<'a>;
+
+    fn next(&mut self) -> Option<Token<'a>> {
+        let patterns = [
+            (TokenTag::NonInukASCII, &NON_INUK_ASCII),
+            (TokenTag::Whitespace, &WHITESPACE),
+            (TokenTag::Skip, &SKIP),
+            (TokenTag::Other, &OTHER),
+        ];
+
+        for (tag, pattern) in patterns {
+            if let Some(result) = pattern.find(self.buffer) {
+                self.buffer = &self.buffer[result.len()..];
+                return Some(Token { tag: tag, substring: result.as_str() });
+            }
+        }
+        None
+    }
+}
+
+#[derive(Debug)]
+pub struct Token<'a> {
+    tag: TokenTag,
+    substring: &'a str
+}
+
+#[derive(Debug)]
+pub enum TokenTag {
+    NonInukASCII,
+    Whitespace,
+    Skip,
+    Other
+}
