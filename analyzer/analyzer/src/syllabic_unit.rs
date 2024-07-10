ad2f70e3a7a7e719a4c823664e841573a2c8df4b
Gaven Rendell <gaven@rendello.ca>
2024-07-10 12:45:42 -0400

    Separate generated maps from handwritten code

---
+++ analyzer/analyzer/src/syllabic_unit.rs
@@ -0,0 +1,59 @@
+
+pub use enumset::{enum_set, EnumSet, EnumSetType};
+use phf_macros::phf_map;
+
+
+#[derive(Debug, EnumSetType)]
+pub enum Dialect {
+    Nunavut,
+    Nattilik,
+    Aivilik,
+    Nunavik,
+}
+
+pub type DialectSet = EnumSet<Dialect>;
+
+
+#[derive(Debug, Clone)]
+pub struct SyllabicUnit {
+    pub dialects: DialectSet,
+    pub consonant: Option<&'static str>,
+    pub vowel: Option<&'static str>,
+    pub original: SyllabicUnitRepresentation,
+}
+
+impl SyllabicUnit {
+
+    /// Perhaps join to existing string?
+    fn normalized_string(&self) -> String {
+        self.consonant.unwrap_or_default().to_owned() 
+        + self.vowel.unwrap_or_default()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum SyllabicUnitRepresentation {
+    Latin(LatinSyllabicUnit),
+    Syllabic(SyllabicSyllabicUnit)
+}
+
+#[derive(Debug, Clone)]
+pub struct LatinSyllabicUnit {
+    pub consonant: Option<&'static str>,
+}
+
+#[derive(Debug, Clone)]
+pub struct SyllabicSyllabicUnit {
+    pub representation: &'static str,
+    pub ai_representation: AiRepresentation
+}
+
+#[derive(Debug, Clone)]
+pub enum AiRepresentation {
+    Split,
+    Classic,
+    Ring,
+    NotApplicable
+}
+
+pub type SyllabicUnitMap = phf::Map<&'static str, &'static SyllabicUnit>;
