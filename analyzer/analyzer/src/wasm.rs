7655256b3100344e8267a029a2a047135b6e7715
Gaven Rendell <gaven@rendello.ca>
2024-07-18 21:02:06 -0400

    Add form input for real time tokenizing on the web

--- analyzer/analyzer/src/wasm.rs
+++ analyzer/analyzer/src/wasm.rs
@@ -8,16 +8,6 @@ fn main() -> Result<(), JsValue> {
 }
 
 
-// #[wasm_bindgen]
-// pub fn convert_to_syl(s: String) -> String {
-//     crate::to_syl(s.to_lowercase().as_str())
-// }
-
-// #[wasm_bindgen]
-// pub fn convert_to_lat(s: String) -> String {
-//     crate::to_lat(s.as_str())
-// }
-
 #[wasm_bindgen]
 pub fn tokenize(s: String) -> String {
     let tokens = Tokenizer::new(&s);
