cb6a6d48c98277bd4410ba37854ea83cd2332669
Gaven Rendell <gaven@rendello.ca>
2024-07-30 13:37:20 -0400

    Let tokenize run on all text nodes

--- inuklib/inuklib/static/extension/common/init.js
+++ inuklib/inuklib/static/extension/common/init.js
@@ -1,13 +1,32 @@
 import init, { tokenize } from './inuklib.js';
 
+function traverse(node, func) {
+  if ( node.nodeType === Node.TEXT_NODE
+    && node.parentElement
+    && getComputedStyle(node.parentElement).display !== 'none'
+    && node.nodeValue.trim() !== '') {
+
+    const wrapper = document.createElement('span');
+    wrapper.innerHTML = func(node.nodeValue);
+    node.parentElement.replaceChild(wrapper, node);
+  }
+
+  for (let child of node.childNodes) {
+    traverse(child, func);
+  }
+}
+
+function inuk() {
+  const body = document.body;
+  traverse(body, tokenize);
+}
+
+
 async function run() {
     await init();
     window.tokenize = tokenize;
-    document.querySelectorAll('p').forEach(element => {
-        if (element.innerText !== null) {  
-            element.innerHTML = tokenize(element.innerText);
-        }
-    });
+    window.inuk = inuk;
 }
 
 run();
+
